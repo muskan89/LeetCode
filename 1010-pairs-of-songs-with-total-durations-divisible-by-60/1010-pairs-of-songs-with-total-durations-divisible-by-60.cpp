751c445c8101b3520b4f1449a3df517dd3edfc43
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-01-19 12:12:25 +0530

    Time: 24 ms (93.63%), Space: 23.3 MB (21.70%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -13,21 +13,16 @@ public:
         int n=collec.size(),ans=0;
         for(int i=0;i<n;i++){
             int y=0;
-            if(collec[i]==0){
-                y=mp[0]-1;
-                y*=mp[0];
+            if(collec[i]==0 || collec[i]==30){
+                y=mp[collec[i]]-1;
+                y*=mp[collec[i]];
                 y/=2;
-                mp[0]=0;
+                mp[collec[i]]=0;
             }
             else if(collec[i]!=30){
                 y=mp[60-collec[i]];
                 mp[60-collec[i]]=0;
                 y*=mp[collec[i]];
-            }else{
-                y=mp[30]-1;
-                y*=mp[30];
-                y/=2;
-                mp[30]=0;
             }
             ans+=y;
         }
