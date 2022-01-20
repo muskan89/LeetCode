79eccb0909a68ce5df881a8c0a9d4a54577d93d2
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-01-20 14:00:56 +0530

    Time: 178 ms (5.10%), Space: 35 MB (6.76%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long int start=1,end=*max_element(piles.begin(),piles.end());
+        int ans=0;
+        while(start <= end){
+            long long int mid=start+((end-start)/2);
+            if(khtmhue(mid,piles,h)){
+                ans=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+    
+    bool khtmhue(long long int k,vector<int>piles, int h){
+        int i=0,n=piles.size();
+        /*while(h>0 && i<n){
+            if(piles[i]>=k){
+                piles[i]/=k;
+                h-=piles[i];
+            }else{
+                piles[i]=0;
+            }
+            if(piles[i]==0){
+                i++;
+            }
+        }
+        if(i>=n){
+            return true;
+        }
+        return false;
+        */
+        while(i<n){
+            if(piles[i]>0){
+                if(piles[i]%k!=0){
+                    h--;
+                }
+                piles[i]/=k;
+                h-=piles[i];
+                piles[i]=0;
+            }
+            i++;
+            if(h<0){
+                return false;
+            }
+            if(h==0 && i<n){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
