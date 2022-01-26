883500a89f771615928e9cb9b17e11dbcd3401bc
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-01-26 13:58:34 +0530

    Time: 16 ms (94.79%), Space: 17.7 MB (62.63%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>candies(n,1);
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(ratings[i-1]<ratings[i] ){
+                candies[i]=candies[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i+1]<ratings[i] && candies[i]<=candies[i+1]){
+                candies[i]=candies[i+1]+1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            ans+=candies[i];
+        }
+        return ans;
+    }
+};
+//[1,3,4,5,2]
+//[1,2,3,4,1]
