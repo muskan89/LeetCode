85660b81d1e25f2a6647c6fa7fc5b7ba3fd1bfb8
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-01-30 12:38:28 +0530

    Time: 53 ms (23.40%), Space: 25.1 MB (52.93%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,17 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        vector<int>vec(n);
-        int h=n-k;
-        for(int i=0;i<k;i++){
-            vec[i]=nums[h+i];
-        }
-        int j=0;
-        for(int i=k;i<n;i++){
-            vec[i]=nums[j++];
-        }
-        for(int i=0;i<n;i++){
-            nums[i]=vec[i];
-        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
