c5d77e9a04e3daccde93d80d72780869fb3cd567
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-02-15 17:35:30 +0530

    Time: 15 ms (40.89%), Space: 7.4 MB (40.10%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int> map_s1(26), map_s2(26);
-		for(char& i1 : s1) 
-			map_s1[i1 - 'a']++;
-		for(int i = 0; i < s2.size(); i++) {
-			map_s2[s2[i] - 'a']++;
-			if(i >= s1.size() - 1) {
-				if(map_s2 == map_s1)  // Time: O(26)
-					return true;
-				map_s2[s2[i - s1.size() + 1] - 'a']--;
-			}
-		}
+        vector<int>f1(26),f2(26);
+        int n1=s1.length();
+        for(int i=0;i<n1;i++){
+            f1[s1[i]-'a']++;
+        }
+        int n2=s2.length();
+        if(n2<n1){
+            return false;
+        }
+        for(int i=0;i<n1;i++){
+            f2[s2[i]-'a']++;
+        }
+        bool ans=true;
+        for(int i=0;i<26;i++){
+            if(f1[i]!=f2[i]){
+                ans=false;
+            }
+        }
+        if(ans)return ans;
+        for(int i=n1;i<n2;i++){
+            f2[s2[i]-'a']++;
+            f2[s2[i-n1]-'a']--;
+            ans=true;
+            for(int j=0;j<26;j++){
+                if(f1[j]!=f2[j]){
+                    ans=false;
+                }
+            }
+            if(ans)return ans;
+        }
+    
 		return false;
     }
+    
 };
