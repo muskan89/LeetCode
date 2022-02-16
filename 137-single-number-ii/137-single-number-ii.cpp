b324568e7e52d12b4fe0cef5f339c8fcd8ed0ae3
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-02-16 13:52:43 +0530

    Time: 8 ms (84.81%), Space: 9.5 MB (58.17%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        if(n==1){
-            return nums[0];
-        }
-        if(nums[0]!=nums[1]){
-            return nums[0];
-        }
-        for(int i=1;i<n-1;i++){
-            if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
-                return nums[i];
+        int ans=0;
+        for(int i=0;i<32;i++){
+            int check=1<<i,k=0,cnt=0;
+            for(int j=0;j<nums.size();j++){ 
+                if(nums[j]&check){
+                    cnt++;
+                }
+            }
+            if(cnt%3){
+                ans|=check;
             }
         }
-        return nums[n-1];
+        return ans;
     }
 };
