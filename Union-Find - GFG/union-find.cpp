5813850a1773d2dcbc1003586539f1864ac9323f
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-06-12 09:34:02 +0530

    Added solution - LeetHub

---
+++ Union-Find - GFG/union-find.cpp
@@ -0,0 +1,94 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to merge two nodes a and b.
+    int checkParent(int par[],int ele){
+        if(par[ele]==ele){
+            return ele;
+        }
+        
+        return checkParent(par,par[ele]);
+    }
+    void union_( int a, int b, int par[], int rank1[]) 
+    {
+        a=checkParent(par,a);
+        b=checkParent(par,b);
+        
+        if(rank1[a]>rank1[b]){
+            par[b]=a;
+        }else if(rank1[a]<rank1[b]){
+            par[a]=b;
+        }else{
+            par[a]=b;
+            rank1[a]++;
+        }
+    }
+    
+    //Function to check whether 2 nodes are connected or not.
+    bool isConnected(int x,int y, int par[], int rank1[])
+    {
+        //code here
+        
+        if(checkParent(par,x)==checkParent(par,y)){
+            return true;
+        }
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    //taking number of testcases
+    int t;
+    cin>>t;
+    while(t--) {
+        
+        //taking number of elements
+        int n;
+        cin>>n;
+        
+        
+        int par[n+1];
+        int rank1[n+1];
+
+        //initializing the parent and
+        //the rank array
+        for(int i=1; i<=n; i++) {
+            par[i] = i;
+            rank1[i] = 1;
+    }
+    
+    //taking number of queries
+    Solution obj;
+    int q;
+    cin>>q;
+    while(q--) {    
+        int a,b;
+        char c;
+        cin>>c>>a>>b;
+        
+        //if query type is 'U'
+        //then calling union_ method
+        if(c=='U')
+        {
+            obj.union_(a,b, par, rank1);
+        }
+        else//else calling isConnected() method
+        cout<<obj.isConnected(a,b, par, rank1)<<endl;
+    }
+    }
+    return 0;
+}
+  // } Driver Code Ends
