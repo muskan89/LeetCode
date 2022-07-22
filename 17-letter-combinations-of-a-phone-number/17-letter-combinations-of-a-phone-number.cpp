d3587a7e7d3442a593da539d4d08f0e8b896b578
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-07-22 20:32:40 +0530

    Time: 3 ms (45.91%), Space: 6.5 MB (83.12%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,68 +1,24 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        unordered_map<char,string>mp;
-        mp['2']="abc";
-        mp['3']="def";
-        mp['4']="ghi";
-        mp['5']="jkl";
-        mp['6']="mno";
-        mp['7']="pqrs";
-        mp['8']="tuv";
-        mp['9']="wxyz";
-        int y=digits.length();
+        map<char,string>mp;
+        vector<string>key{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        string temp;
         vector<string>ans;
-        if(y==0){
-            return ans;
-        }else if(y==1){
-            for(char c:mp[digits[0]]){
-                string h;
-                h+=c;
-                ans.push_back(h);
-            }
-        }else if(y==2){
-            for(char c:mp[digits[0]]){
-                string h;
-                h+=c;
-                h+='a';
-                for(char d:mp[digits[1]]){
-                    h[1]=d;
-                    ans.push_back(h);
-                }   
-            }
-        }else if(y==3){
-            for(char c:mp[digits[0]]){
-                string h;
-                h+=c;
-                h+='a';
-                h+='a';
-                for(char d:mp[digits[1]]){
-                    h[1]=d;
-                    for(char e:mp[digits[2]]){
-                        h[2]=e;
-                        ans.push_back(h);
-                    }
-                }   
-            }
-        }else if(y==4){
-            for(char c:mp[digits[0]]){
-                string h;
-                h+=c;
-                h+='a';
-                h+='a';
-                h+='a';
-                for(char d:mp[digits[1]]){
-                    h[1]=d;
-                    for(char e:mp[digits[2]]){
-                        h[2]=e;
-                        for(char f:mp[digits[3]]){
-                            h[3]=f;
-                            ans.push_back(h);
-                        }
-                    }
-                }   
-            }
-        }
+        solve(0,temp,ans,digits,key);
         return ans;
+    }
+    
+    void solve(int start,string &temp,vector<string>&ans,string &digits,vector<string>&key){
+        if(start==digits.length()){
+            if(temp.length()>0)
+                ans.push_back(temp);
+            return;
+        }
+        for(int i=0;i<key[digits[start]-'0'].length();i++){
+            temp+=key[digits[start]-'0'][i];
+            solve(start+1,temp,ans,digits,key);
+            temp=temp.substr(0,temp.length()-1);
+        }
     }
 };
