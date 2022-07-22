4ca4815d3c11bfb8c5fade701644fd85a5d92784
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-07-22 14:53:23 +0530

    Time: 3 ms (98.00%), Space: 11.2 MB (56.53%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target)     {
-        sort(candidates.begin(),candidates.end());
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
         vector<int>temp;
-        int start=0;
-        solve(candidates,temp,target,start);
+        sort(candidates.begin(),candidates.end());
+        solve(0,temp,ans,candidates,target);
         return ans;
     }
-    void solve(vector<int>& candidates,vector<int>&temp, int target,int start){
-        if(target == 0){
+    
+    void solve(int start,vector<int>&temp,vector<vector<int>>&ans,vector<int>& candidates, int target){
+        if(target==0){
             ans.push_back(temp);
+        }else if(target<0){
             return;
         }
-        
-        for(int i=start;i<candidates.size() && target>=candidates[i];i++){
-            temp.push_back(candidates[i]);
-            solve(candidates,temp,target-candidates[i],i);
-            temp.pop_back();   
+        for(int i=start;i<candidates.size();i++){
+            if(candidates[i]<=target){
+                temp.push_back(candidates[i]);
+            }else{
+                break;
+            }
+            solve(i,temp,ans,candidates,target-candidates[i]);
+            if(temp.size()){
+                temp.pop_back();
+            }
         }
     }
 };
