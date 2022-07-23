b7e9a552e7168243aef48af1e4185291b1951386
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-07-24 00:45:49 +0530

    Time: 7 ms (23.23%), Space: 7.5 MB (90.93%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -14,27 +14,14 @@ public:
         if(head==NULL || head->next==NULL){
             return head;
         }
-        ListNode* prev=NULL,*curr=NULL,*nxt=NULL,*before=NULL;
-        ListNode* dummy=new ListNode(0);
-        dummy->next=head;
-        before=dummy;
-        while(head!=NULL){
-            if(head->next==NULL){
-                break;
-            }
-            curr=head;
-            nxt=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=nxt;
-            nxt=curr->next;
-            curr->next=prev;
-            head->next=nxt;
-            before->next=curr;
-            before=head;
-            head=before->next;
-            prev=NULL;    
-        }
-        return dummy->next;
+        ListNode* curr=head,*prev=NULL,*nxt=NULL;
+        nxt=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nxt;
+        nxt=curr->next;
+        curr->next=prev;
+        head->next=swapPairs(nxt);
+        return curr;
     }
 };
