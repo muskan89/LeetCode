1bd4cf7b5521c7605fa709c340cc6f8c2a41ee7b
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-07-25 23:58:06 +0530

    Time: 31 ms (26.45%), Space: 11.6 MB (29.90%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -11,31 +11,29 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* dummy=new ListNode(0),*before=NULL;
-        dummy->next=head;
-        before=dummy;
-        ListNode* after=head,*curr=NULL,*prev=NULL,*nxt=NULL;
-        while(true){
-            ListNode* cursor=after;
-            for(int i=0;i<k;i++){
-                if(cursor==NULL){
-                    return dummy->next;
-                }
-                cursor=cursor->next;
-            }
-            curr=after;
-            for(int i=0;i<k;i++){
-                nxt=curr->next;
-                curr->next=prev;
-                prev=curr;
-                curr=nxt;
-            }
-            //cout<<before->val<<"\n";
-            after->next=curr;
-            before->next=prev;
-            before=after;
-            after=curr;
+        if(head==NULL || head->next==NULL){
+            return head;
         }
-        return dummy->next;
+        ListNode* curr=head,*prev=NULL,*next=NULL;
+        int num=k,cnt=0;
+        ListNode* check=head;
+        while(check!=NULL){
+            cnt++;
+            if(cnt>=k){
+                break;
+            }
+            check=check->next;
+        }
+        if(cnt<k){
+            return head;
+        }
+        while(curr!=NULL && num--){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head->next=reverseKGroup(curr,k);
+        return prev;
     }
 };
