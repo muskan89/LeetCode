897ed84d95f13fa17fa555edba02b5eefd9fe6d0
Muskan Gupta <67742819+muskan89@users.noreply.github.com>
2022-09-10 02:04:13 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (99.34%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -8,14 +8,14 @@ public:
         if(n>=2){
             ans.push_back({1,1});
         }
+        vector<int>k(n);
         for(int i=2;i<n;i++){
-            vector<int>k(i+1);
             k[0]=1;
             k[i]=1;
             for(int j=1;j<i;j++){
                 k[j]=ans[i-1][j]+ans[i-1][j-1];
             }
-            ans.push_back(k);
+            ans.push_back(vector<int>(k.begin(),k.begin()+i+1));
         }
         return ans;
         
